c08e4be398a14b942606c33c2510c7167b7ce47d
faisalalamsyah <gardenapparel049@gmail.com>
2018-07-20 08:30:40 +0700

    v2.1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+
 
 @Component({
   selector: 'app-home',
@@ -7,12 +7,7 @@ import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- closeResult: string;
-  constructor(private modalService:NgbModal) { }
 
-  openLg(content) {
-    this.modalService.open(content, { size: 'lg' });
-  }
 
   ngOnInit() {
   }
